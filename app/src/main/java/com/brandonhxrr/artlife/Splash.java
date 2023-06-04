231667a2b93ec67024f4493cc9ca7c6b3dd06c08
Brandon Herrera <brandondh769@gmail.com>
2023-06-03 20:33:25 -0600

    Add splash timer

--- app/src/main/java/com/brandonhxrr/artlife/Splash.java
+++ app/src/main/java/com/brandonhxrr/artlife/Splash.java
@@ -2,7 +2,9 @@ package com.brandonhxrr.artlife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new CountDownTimer(2000, 1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent startIntro = new Intent(Splash.this, Intro.class);
+                startActivity(startIntro);
+            }
+        }.start();
     }
 }
