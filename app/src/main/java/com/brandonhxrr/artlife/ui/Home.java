d1ee13a8d0afc60388f5678f12441e126616ed4b
Brandon Herrera <brandondh769@gmail.com>
2023-06-09 13:14:56 -0600

    Add firebase parameter in home fragment

--- app/src/main/java/com/brandonhxrr/artlife/ui/Home.java
+++ app/src/main/java/com/brandonhxrr/artlife/ui/Home.java
@@ -7,8 +7,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.brandonhxrr.artlife.R;
 import com.brandonhxrr.artlife.ui.intro.IntroFragment;
+import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Home extends Fragment {
+
+    MaterialTextView textHello;
+    CircleImageView profileImage;
+    FirebaseUser currentUser;
 
     public Home() {}
     public static Home newInstance() {
@@ -18,12 +27,22 @@ public class Home extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+       textHello = view.findViewById(R.id.txt_hello);
+       profileImage = view.findViewById(R.id.profile_image);
+
+       textHello.setText("Hola, " + currentUser.getDisplayName());
+       if(currentUser.getPhotoUrl() != null){
+           profileImage.setImageURI(currentUser.getPhotoUrl());
+       }
 
        return view;
     }
