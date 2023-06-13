f45ee0c01106235ba2850d40d02fe3dd1b690ba4
Brandon Herrera <brandondh769@gmail.com>
2023-06-12 22:23:41 -0600

    Add blogs fragment

---
+++ app/src/main/java/com/brandonhxrr/artlife/ui/Blogs.java
@@ -0,0 +1,27 @@
+package com.brandonhxrr.artlife.ui;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.brandonhxrr.artlife.R;
+
+public class Blogs extends Fragment {
+
+    public Blogs() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blogs, container, false);
+        return view;
+    }
+}
