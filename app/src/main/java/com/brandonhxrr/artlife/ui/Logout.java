c90c44294600c603f3afc1b46fec7d513f0a27eb
MoisesVanegasC <moises_9_cristiano@hotmail.com>
2023-06-16 17:30:42 -0600

    Se agrego UI de pintura y UI de Logout

---
+++ app/src/main/java/com/brandonhxrr/artlife/ui/Logout.java
@@ -0,0 +1,16 @@
+package com.brandonhxrr.artlife.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.brandonhxrr.artlife.R;
+
+public class Logout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+    }
+}
